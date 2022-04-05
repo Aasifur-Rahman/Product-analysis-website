4c18d33e2821b33c1389aa08a89e2398d17ba1a7
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-04-05 16:41:57 +0600

    nav bar commited

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>
+                    <h1>Here is your the next go to camera</h1>
+                    <h2>Best in the budget</h2>
+                    <p>Sony Alpha A600 mirrorless digital camera with 15-50mm lens is the go to camera for all.You can take cool pictures with the 24.3-megapixel APS-C image sensor.It also has High-resolution OLED Tru-Finder.Over all this is a budget beast.</p>
+                </div>
+
+                <div className='col-6'>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
