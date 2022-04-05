dc1e15fde0c4a009198a7dcfb3fc4decd0d626b3
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-04-05 19:52:57 +0600

    review part complete

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,10 +4,10 @@ const Review = (props) => {
     const { name, photo, id, comment, rating } = props.review;
     return (
         <div>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div>
                 <div className="col">
-                    <div className="card h-100">
-                        <img src={photo} alt="" />
+                    <div className="card h-100 ">
+                        <img className='p-3' width='100%' src={photo} alt="" />
                         <div className="card-body">
                             <p><small className='text-muted'>ID: {id}</small></p>
                             <h5 className="card-title">Name: {name}</h5>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -6,9 +6,9 @@ const Reviews = () => {
     const [reviews, setReviews] = useReview();
 
     return (
-        <div>
-            <h2>This is Review Section of Camera World</h2>
-            <div>
+        <div className='container'>
+            <h2 className='mt-4'>What our customer says!</h2>
+            <div className="mt-3 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                 {
                     reviews.map(review => <Review
                         key={review.id}
